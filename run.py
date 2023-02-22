15559f7143938277d3c31dfbfd4c1d284bedc5cc
Briar <briar.j.mills@gmail.com>
2023-02-22 09:37:22 -0500

    Change writing to csv

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-import csv
 import logging
 import warnings
 from shapely.errors import ShapelyDeprecationWarning
@@ -6,6 +5,7 @@ from shapely.errors import ShapelyDeprecationWarning
 from hdx.data.dataset import Dataset
 from hdx.facades.keyword_arguments import facade
 from hdx.utilities.easy_logging import setup_logging
+from hdx.utilities.dictandlist import write_list_to_csv
 from hdx.utilities.path import temp_dir
 
 from check_location import check_location, get_global_pcodes
@@ -20,48 +20,40 @@ def main(**ignore):
                          "shp", "topojson", "xls", "xlsx"]
 
     with temp_dir(folder="TempLocationExploration") as temp_folder:
-        datasets = Dataset.search_in_hdx(
-            fq='cod_level:"cod-standard"'
-        ) + Dataset.search_in_hdx(fq='cod_level:"cod-enhanced"')
+        datasets = Dataset.search_in_hdx(fq='groups="tur"')
         logger.info(f"Found {len(datasets)} datasets")
 
         global_pcodes = get_global_pcodes(
             "https://raw.githubusercontent.com/b-j-mills/hdx-global-pcodes/main/global_pcodes.csv"
         )
 
-        with open("datasets_location_status.csv", "w") as c:
-            writer = csv.writer(c)
-            writer.writerow([
-                "dataset name",
-                "resource name",
-                "pcoded",
-                "latlonged",
-                "error",
-            ])
+        status = [["dataset name", "resource name", "pcoded", "latlonged", "error"]]
 
-            for dataset in datasets:
-                logger.info(f"Checking {dataset['name']}")
+        for dataset in datasets:
+            logger.info(f"Checking {dataset['name']}")
 
-                resources = dataset.get_resources()
-                for resource in resources:
-                    if resource.get_file_type() not in allowed_filetypes:
-                        writer.writerow([
-                            dataset["name"],
-                            resource["name"],
-                            None,
-                            None,
-                            f"{resource.get_file_type()} can't be p-coded",
-                        ])
-                        continue
-
-                    pcoded, latlonged, error = check_location(resource, global_pcodes, temp_folder)
-                    writer.writerow([
+            resources = dataset.get_resources()
+            for resource in resources:
+                if resource.get_file_type() not in allowed_filetypes:
+                    status.append([
                         dataset["name"],
                         resource["name"],
-                        pcoded,
-                        latlonged,
-                        error,
+                        None,
+                        None,
+                        f"{resource.get_file_type()} can't be p-coded",
                     ])
+                    continue
+
+                pcoded, latlonged, error = check_location(resource, global_pcodes, temp_folder)
+                status.append([
+                    dataset["name"],
+                    resource["name"],
+                    pcoded,
+                    latlonged,
+                    error,
+                ])
+
+        write_list_to_csv("datasets_location_status.csv", status)
 
 
 if __name__ == "__main__":
